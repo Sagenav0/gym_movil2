924edf9a2cdf9590e180cba77404902c4b7f8cae
franco1123 <yefercuesta123@gmail.com>
2024-03-15 22:03:48 -0500

    interfaz contactanos funcional con consulta ala bd y scss

--- src/app/contactanos/contactanos.page.ts
+++ src/app/contactanos/contactanos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConexionService } from '../services/conexion.service';
 
+import { from } from 'rxjs';
 @Component({
   selector: 'app-contactanos',
   templateUrl: './contactanos.page.html',
@@ -16,15 +17,28 @@ export class ContactanosPage implements OnInit {
   }
 
   obtenerDatosGym() {
-    // Llama al servicio para obtener los datos
     this.conexionService.datosGym().subscribe(
       (datos: any[]) => {
-        this.listaDeDatos = datos;
+        // guardamos los datos y convertimos todos los datos a mayúsculas
+        this.listaDeDatos = datos.map(item => this.convertirObjetoAMayusculas(item));
       },
       (error) => {
         console.error('Error al obtener los datos:', error);
-        // Aquí puedes agregar código para manejar el error, como mostrar un mensaje al usuario
+        // Aquí podemos agregar codigo para manejar el errores o mostrar un mensaje al usuario
       }
     );
+  }
+
+  convertirObjetoAMayusculas(objeto: any): any {
+    const nuevoObjeto: any = {}; // Se define el tipo any para nuevoObjeto
+    // Iterar sobre las propiedades del objeto
+    for (let key in objeto) {
+      // Verificar que la propiedad sea propia del objeto 
+      if (objeto.hasOwnProperty(key)) {
+        // Convertir el valor de la propiedad a mayúsculas si es una cadena, de lo contrario, mantenerlo igual
+        nuevoObjeto[key] = typeof objeto[key] === 'string' ? objeto[key].toUpperCase() : objeto[key];
+      }
+    }
+    return nuevoObjeto;
   }
 }
